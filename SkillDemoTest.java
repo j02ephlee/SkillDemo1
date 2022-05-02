c22a5b5d8179b3c4c82db8da3536a215a6a9e39a
j02ephlee <jul050@ucsd.edu>
2022-05-01 21:05:45 -0700

    added test files

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void TwoTimesThreeShouldEqualSix() {
+        assertEquals(6, SkillDemo.multiply(3, 1));
+    }
+}
